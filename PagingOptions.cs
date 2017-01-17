62e695fd4917ffdefd5e67f803604f62ee02c1c6
RajputVikashRana <vikash.kumar@netsolutions.in>
2017-01-17 16:46:24 +0530

    Paging Property
    It is containing paging property to use in js file with c#

---
+++ PagingOptions.cs
@@ -0,0 +1,48 @@
+﻿namespace Models.Core
+{
+    /// <summary>
+    /// Represents a paging option
+    /// </summary>
+    public class PagingOptions
+    {
+        /// <summary>
+        /// Gets or sets the SortField parameter
+        /// </summary>
+        public string SortField { get; set; }
+
+        /// <summary>
+        /// Gets or sets the SortOrder value for sort order
+        /// </summary>
+        public string SortOrder { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Page Index for paging 
+        /// </summary>
+        public int? PageIndex { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Page Size total number record display
+        /// </summary>
+        public int? PageSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total number record
+        /// </summary>
+        public int RecordCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets the total number PAGE
+        /// </summary>
+        public int PageCount { get; set; }
+
+        /// <summary>
+        /// Gets the position of the first record to be displayed.
+        /// </summary>        
+        public int? LowerBound => ((PageIndex - 1) * PageSize) + 1;
+
+        /// <summary>
+        /// Gets the position of the last record to be displayed.
+        /// </summary>
+        public int? UpperBound => (LowerBound + PageSize) > RecordCount ? RecordCount : (LowerBound + PageSize);
+    }
+}
